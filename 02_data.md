966fb3159602a02cdb3cfdb45e0acf8cc7dc0543
trenkmann <maximilian.trenkmann@gmail.com>
2020-06-24 17:00:43 +0200

    Add Filter to Datatable

--- 02_data.md
+++ 02_data.md
@@ -1,47 +1,49 @@
 ---
-layout: page
+layout: datatable
 title: Data
 permalink: /data/
 order: 2
 bodyclass: page-datatable
 ---
 
-<!-- {{ site.data.trackers }} -->
-
 <table>
   {% for row in site.data.trackers %}
     {% if forloop.first %}
-    <tr>
-      <td>Title</td>
-      <td>Category</td>
-      <td>Focus</td>
-      <td>Coverage</td>
-      <td>Data Format</td>
-      <td>By</td>
-    </tr>
+    <thead>
+      <tr>
+        <td>Title</td>
+        <td>Category</td>
+        <td>Focus</td>
+        <td>Coverage</td>
+        <td>Data Format</td>
+        <td>By</td>
+      </tr>
+    </thead>
     {% else %}
-    <tr>
-      <td>
-        <a href="{{row['link']}}">
-          {{ row['title'] }}
-        </a>
-      </td>
-      <td>
-        {{ row['category'] }}
-      </td>
-      <td>
-        {{ row['Focus'] }}
-      </td>
-      <td>
-        {{ row['Coverage'] }}
-      </td>
-      <td>
-        {{ row['Data format'] }}
-      </td>
-      <td>
-        {{ row['By'] }}
-      </td>
-    </tr>
+    <tbody>
+      <tr>
+        <td>
+          <a href="{{row['link']}}">
+            {{ row['title'] }}
+          </a>
+        </td>
+        <td class="category">
+          {{ row['category'] }}
+        </td>
+        <td class="focus">
+          {{ row['Focus'] }}
+        </td>
+        <td class="coverage">
+          {{ row['Coverage'] }}
+        </td>
+        <td class="data-format">
+          {{ row['Data format'] }}
+        </td>
+        <td class="by">
+          {{ row['By'] }}
+        </td>
+      </tr>
+    </tbody>
     {% endif %}
   {% endfor %}
 </table>
--- assets/js/datatable.js
+++ assets/js/datatable.js
@@ -1,1 +1,64 @@
-console.log('datatable.js')
+jQuery(function () {
+  jQuery("#search_filter").on('keyup change',function (event) {
+    dt_filter(jQuery(this).val());
+  });
+  jQuery("#category_filter").on('change',function (event) {
+    dt_filter(jQuery(this).val());
+  });
+  jQuery("#coverage_fitler").on('change',function (event) {
+    dt_filter(jQuery(this).val());
+  });
+  jQuery("#dataformat_filter").on('change',function (event) {
+    dt_filter(jQuery(this).val());
+  });
+  
+  const table = jQuery(".datatable-container table");
+  const rows = table.find("tbody tr");
+  let categories = new Set();
+  let coverage = new Set();
+  let dataFormat = new Set();
+  
+  rows.each(function(){
+    const row = jQuery(this);    
+
+    let c = row.find('td.category').text();
+    c = c.split(',').map(elem=>elem.trim());
+    categories = new Set([...categories, ...c]);
+    
+    let cov = row.find('td.coverage').text();
+    cov = cov.split(';').map(elem=>elem.trim());
+    coverage = new Set([...coverage, ...cov]);
+
+    let d = row.find('td.data-format').text();
+    d = d.split(';').map(elem=>elem.trim());
+    dataFormat = new Set([...dataFormat, ...d]);
+  })
+
+  categories.forEach((elem)=>{
+    jQuery('#category_filter').append('<option>'+elem+'</option>')
+  })
+  coverage.forEach((elem)=>{
+    jQuery('#coverage_filter').append('<option>'+elem+'</option>')
+  })
+  dataFormat.forEach((elem)=>{
+    jQuery('#dataformat_filter').append('<option>'+elem+'</option>')
+  })
+});
+
+function dt_filter(filters) {
+  if (!Array.isArray(filters)) {
+    filters = [filters];
+  }
+  const table = jQuery(".datatable-container table");
+  const rows = table.find("tbody tr");
+  rows.each(function () {
+    textContent = this.textContent.toUpperCase();
+    const show = filters.every((term) => textContent.includes(term.toUpperCase()));
+    console.log(show);
+    if (!show) {
+      jQuery(this).hide();
+    } else {
+      jQuery(this).show();
+    }
+  });
+}
